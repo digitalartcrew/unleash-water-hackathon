8048cd2c093db35ae13740bac0cb7bb6b0cad393
Donte Burney <donteburney@Dontes-Mac-mini.local>
2022-04-08 12:47:31 -1000

    Updates

--- server/models/index.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require("mongoose");
-mongoose.connect(
-  process.env.MONGOLAB_URI || "mongodb://localhost/water-alarm-app"
-);
-
-mongoose.set("debug", true);
-
-module.exports.Alarm = require("./alarm");
--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-const authRoutes = require("../routes/authRoutes");
-const alarmRoutes = require("../routes/alarmRoutes");
-
-const routes = {
-  authRoutes,
-  alarmRoutes,
-};
-
-module.exports = routes;
